247558f64a33bc89d71d11a2d0ec1f8cadec0884
genovarino <genovarino@gmail.com>
2024-03-01 12:07:03 -0300

    modificaciones sobre index,script y style

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
- //asignamos a campo_texto el valor del texto escrito en campo de entrada
-const campo_texto = document.querySelector("#texto-encriptado");
+ //asignamos a campo_entrada el valor del texto escrito en campo de entrada
+const campo_entrada = document.querySelector("#texto-a-encriptar");
 
-//asignamos a campo_mensaje el valor del texto obtenido en campo encriptado
-const campo_mensaje = document.querySelector("#campo-mensaje");
+//asignamos a campo_encriptado el valor del texto obtenido en campo encriptado
+const campo_encriptado = document.querySelector("#texto-encriptado");
 
 //definimos el arreglo que se ocupara de relacionar la posicion 0 y 1 de todos los indices
 const matriz_code = [
@@ -16,9 +16,9 @@ const matriz_code = [
 // declaramos una funcion que captura el evento del boton "encriptar"
 function btnEncriptar(){
     //dentro de este boton , llamaremos a la funcion encriptarTexto
-    const texto = encriptarTexto(campo_texto.value);
+    const texto = encriptarTexto(campo_entrada.value.toLowerCase() );
     //hacemos que el campo 2 recib el valor del texto encriptado
-   campo_mensaje.value=texto;
+   campo_encriptado.value=texto;
 }
 
 //declaramos una funcion que se encarga de encriptar basada en el arreglo matriz_code
@@ -30,5 +30,29 @@ function encriptarTexto(fraseEncriptada){
             );
         }
     }
-    return fraseEncriptada;
+    campo_entrada.value=""; //borramos el campo de entrada
+    return fraseEncriptada; //devolvemos la variable  con el texto encriptado
+    
+}
+
+// declaramos una funcion que captura el evento del boton "desencriptar"
+function btnDesencriptar(){
+    //dentro de este boton , llamaremos a la funcion desEncriptar
+    const texto = desEncriptar(campo_encriptado.value);
+    //hacemos que el campo 1 reciba el valor del texto desencriptado
+   campo_entrada.value=texto;
+}
+
+//declaramos una funcion que se encarga de desencriptar basada en el arreglo matriz_code
+function desEncriptar(fraseDesencriptada){
+    for(let i=0; i < matriz_code.length; i++){
+        if(fraseDesencriptada.includes(matriz_code[i][1])){
+            fraseDesencriptada = fraseDesencriptada.replaceAll(
+                matriz_code[i][1], matriz_code[i][0]
+            );
+        }
+    }
+    campo_encriptado.value=""; //borramos el campo encriptado
+    return fraseDesencriptada; //devolvemos la variable  con el texto desencriptado
+    
 }
